c5c9444b48ba45db89280a9b6e320b546bbdce84
WEI <wwenba2k@gmail.com>
2024-02-04 17:55:28 +0900

    fix:config

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: '/live-vue3-training-chapter-2023/week6/dist/',
+  base: '/2024-vue-week6/dist/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
